da233ff8005b15a0c1a6a03c300e132a0f70a286
Addy Santo <addys@facebook.com>
2016-03-01 17:51:17 +0200

    ...

--- server.js
+++ server.js
@@ -115,5 +115,34 @@ function find(arr, id)
 
 function save(sections)
 {
-    
+    var DocumentClient = require('documentdb').DocumentClient;
+
+    var host = 'https://addys.documents.azure.com:443/';                     // Add your endpoint
+    var masterKey = 'QpkhBVI9257YVvciIfbu3pFrjNVNZPl79Tvsoaa29wYwUNgaSqOdGbWFjArShwcAMXH59P7TEzI1Yxn+5NC1NQ==';  // Add the massterkey of the endpoint
+
+
+    var client = new DocumentClient(host, {masterKey: masterKey});
+    var databaseDefinition = { id: "mmsaDB" };
+    var collectionDefinition = { id: "saved" };
+    var documentDefinition = { id: "hello world doc", content: "Hello World!" };
+
+
+    client.createDatabase(databaseDefinition, function(err, database) {
+    if(err) return console.log(err);
+    console.log('created db');
+
+
+    client.createCollection(database._self, collectionDefinition, function(err, collection) {
+        if(err) return console.log(err);
+        
+        console.log('created collection');
+        
+        client.createDocument(collection._self, documentDefinition, function(err, document) {
+            if(err) return console.log(err);
+            
+            console.log('Created Document with content: ', document.content);
+            cleanup(client, database);
+        });
+    });
+        
 }
